734a348546c8af1dc300fa86f370aacce6c10fda
ooooonnnnn <79533443+ooooonnnnn@users.noreply.github.com>
2026-04-15 14:32:25 +0300

    packages

---
+++ Assets/Scripts/Manager/PersistentSingleton.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+
+[Tooltip("A singleton that will be destroyed if an older one with the same name exists")]
+public class PersistentSingleton : MonoBehaviour
+{
+    private static Dictionary<string, PersistentSingleton> singletonInstances = new();
+    public static PersistentSingleton Instance => _thisInstance;
+    protected static PersistentSingleton _thisInstance;
+
+    protected virtual void Awake()
+    {
+        // print($"Awake called on PersistentSingleton on {gameObject.name}");
+        
+        var gameObjectName = gameObject.name;
+        
+        if (!singletonInstances.ContainsKey(gameObjectName))
+        {
+            singletonInstances.Add(gameObjectName, this);
+            DontDestroyOnLoad(gameObject);
+            _thisInstance = this;
+            return;
+        }
+        
+        if (singletonInstances[gameObjectName] != this) Destroy(gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        if (singletonInstances[gameObject.name] == this) singletonInstances.Remove(gameObject.name);
+    }
+}
