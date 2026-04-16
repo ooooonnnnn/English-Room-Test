bb2f495d272916ee92e4ccfb031b3b5a947f2d52
ooooonnnnn <79533443+ooooonnnnn@users.noreply.github.com>
2026-04-16 20:24:01 +0300

    toggle shop menu

---
+++ Assets/Scripts/ToggleOnAction.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ToggleOnAction : MonoBehaviour
+{
+    [SerializeField] private InputActionReference inputAction;
+
+    private void Awake()
+    {
+        inputAction.action.performed += ToggleActive;
+    }
+
+    public void ToggleActive(InputAction.CallbackContext ctx)
+    {
+        print("toggling");
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+}
